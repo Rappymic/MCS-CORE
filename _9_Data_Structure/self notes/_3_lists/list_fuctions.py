71fa8f2de06919d2b7a555c6eb631229f48afc3e
Rappymic <91956790+Rappymic@users.noreply.github.com>
2021-10-20 15:31:38 +0530

    20 oct

--- _9_Data_Structure/self notes/_3_lists/list_fuctions.py
+++ _9_Data_Structure/self notes/_3_lists/list_fuctions.py
@@ -1,14 +1,53 @@
 print("Append Operation".center(80, "-"))
-l1 = [1,2,3,4]
+l1 = [1, 2, 3, 4]
 x = id(l1)
-
+print("\n")
 print("original list and its id".ljust(40, "-"), l1, x)
 l1.append(5)
 y = id(l1)
 print("modified list and its id".ljust(40, "-"), l1, y)
-print("is the id same".ljust(40, "-"),x==y, "\n")
+print("is the id same".ljust(40, "-"), x == y, "\n")
 
 print("Insert operation".center(80, "-"))
+print("\n")
 print("list and its id".ljust(40, "-"), l1, id(l1))
 l1.insert(2, 90)
 print("list and its id".ljust(40, "-"), l1, id(l1))
+print("\n")
+print("Sorting Reverse".center(80, "-"))
+print("\n")
+print("original list".ljust(40, "-"), l1)
+l1.sort(reverse=True)
+print("reversed list".ljust(40, "-"), l1)
+print("original list".ljust(40, "-"), l1)
+print("\n")
+print("Sorted Function".center(80, "-"))
+print("\n")
+print("original list".ljust(40, "-"), l1)
+l2 = sorted(l1)
+print("sorted list".ljust(40, "-"), l2)
+print("original list".ljust(40, "-"), l1)
+print("\n")
+print("shallow copy".center(80, "-"))
+print("\n")
+new_list = [1, 2, 3, [4, 5, 6, 7]]
+copied_list = new_list.copy()
+print("Original List".ljust(40, "-"), new_list)
+print("copied list".ljust(40, "-"), copied_list)
+print("element [3][2] is being modified")
+new_list[3][2] = "mdr"
+print("Original List".ljust(40, "-"), new_list)
+print("copied list".ljust(40, "-"), copied_list)
+
+from copy import deepcopy
+print("\n")
+print("Deep Copy".center(80, "-"))
+print("\n")
+new_list = [1, 2, 3, [4, 5, 6, 7]]
+copied_list = deepcopy(new_list)
+print("Original List".ljust(40, "-"), new_list)
+print("copied list".ljust(40, "-"), copied_list)
+print("element [3][2] is being modified")
+new_list[3][2] = "mdr"
+print("Original List".ljust(40, "-"), new_list)
+print("copied list".ljust(40, "-"), copied_list)
---
+++ _9_Data_Structure/self notes/_4_Dict/dictionary_functions.py
@@ -0,0 +1,33 @@
+dict1 = {'a': 23, 'b': 34, 'c': 45}
+dict2 = {'d': 434, 'e': 332, 'f': 98}
+print("Adding data".center(80, "-"), "\n")
+print("Original Dictionary".ljust(40, "-"), dict1)
+del dict1['b']
+print("after element update".ljust(40, "-"), dict1)
+dict1['g'] = 82
+print("adding element".ljust(40, "-"), dict1)
+print(f"the value of 'a' key is".ljust(40, "-"), dict1['a'])
+dict1['a'] = "test"
+dict3 = dict1.copy()
+print(f"update the value of 'a'".ljust(40, "-"), dict1)
+dict1.clear()
+print("clearing the dictionary".ljust(40, "-"),dict1)
+del dict1
+try:
+    print("Deleted dictionary", dict1)
+except Exception as e:
+    print("Dictionary is deleted and", e)
+
+print("dictionary methods".center(80, "-"))
+print("dictionary .keys function".ljust(40, "-"), dict3.keys())
+print("dictionary .values function".ljust(40, "-"), dict3.values())
+print("updating the dictionary with two dictionaries".center(80, "-"))
+print("dictionary 1".ljust(40, "-"), dict3)
+print("dictionary 2".ljust(40, "-"), dict2)
+dict3.update(dict2)
+print(f"{dict3}".center(70))
+print("Retrieving values".center(80, "-"))
+print("getting value of a ".ljust(40, "-"), dict3.get('a'))
+print("getting value of an absent key ".ljust(40, "-"), dict3.get(1))
+print("set default function".center(80, "-"))
+print("checking if a is present ".ljust(40, "-"), dict3.setdefault("a","testing"))
