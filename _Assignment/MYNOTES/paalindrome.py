4cac01036e55898a206a60223bc377e07ed145db
Rappymic <91956790+Rappymic@users.noreply.github.com>
2021-10-26 17:49:53 +0530

    rename

--- _Assignment/MYNOTES/paalindrome.py
+++ _Assignment/MYNOTES/paalindrome.py
@@ -2,12 +2,12 @@ def check_palindrome(value):
     """only give whole number as value"""
     try:
         value = int(value)
-    except Exception as E:
+    except Exception:
         return False
     value = str(value)
     result = []
     for i, char in enumerate(value):
-        if value[i] == value[-i-1]:
+        if value[i] == value[-i - 1]:
             result.append(1)
         else:
             result.append(0)
@@ -16,6 +16,6 @@ def check_palindrome(value):
             return False
     else:
         return True
+
+
 print(check_palindrome("12321"))
-
-
