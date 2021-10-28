cc8ff13ebad74775000a41b21978022caea74336
Rappymic <91956790+Rappymic@users.noreply.github.com>
2021-10-28 09:47:57 +0530

    Update factorial.py

--- _0_Assignment/MYNOTES/factorial.py
+++ _0_Assignment/MYNOTES/factorial.py
@@ -1,19 +1,22 @@
 def factorial(value):
     if value == 0 or value == 1:
         return 1
-    if value < 0 :
+    if value < 0:
         return 0
     else:
         fact = value * factorial(value - 1)
         return fact
 
+
 def factorial_iter(val):
     result = 1
     if val >= 0:
-        for i in range(1, val+1):
-            result = result*i
+        for i in range(1, val + 1):
+            result = result * i
         return result
     else:
         return 0
-print("The factorial of 5 is ",factorial(5))
+
+
+print("The factorial of 5 is ", factorial(5))
 print(factorial_iter(5))
