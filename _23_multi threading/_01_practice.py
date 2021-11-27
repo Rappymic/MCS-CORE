9a2322f21e7187125f6b7a98df4d44769c122a0c
Rappymic <91956790+Rappymic@users.noreply.github.com>
2021-11-27 10:25:01 +0530

    27

--- _23_multi threading/_01_practice.py
+++ _23_multi threading/_01_practice.py
@@ -1,7 +1,7 @@
 
 import threading
 
-print("current thread:", threading.current_thread().getName())
+# print("current thread:", threading.current_thread().getName())
 
 if threading.current_thread() == threading.main_thread():
     print("The current thread is the main thread")
